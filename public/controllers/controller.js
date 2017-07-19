e3a8a5828a04411cf08656799efa80b10f21237c
Viswada Yangala <yangalav@go.stockton.edu>
2017-07-19 02:02:48 -0400

    dynamically updates total

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,12 +3,14 @@ myApp.controller('AppCtrl', ['$scope',
     $scope.haha = "cheeseburger";
     $scope.members = [];
     $scope.member = '';
+    $scope.total = 179;
 
     $scope.addMember = function(){
       console.log('submitting');
       if($scope.member) {
         $scope.members.push(this.member);
         $scope.member = '';
+        $scope.total += 50;
       }
     };
 
