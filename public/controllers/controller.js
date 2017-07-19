f8a79d2c7ad7b9adb78966e03bf7dc15f3fdcb8a
Viswada Yangala <yangalav@go.stockton.edu>
2017-07-19 04:27:36 -0400

    able to fetch promocode json data

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,10 +1,11 @@
-myApp.controller('AppCtrl', ['$scope',
-  function($scope) {
+myApp.controller('AppCtrl', ['$scope', '$http',
+  function($scope, $http) {
     $scope.haha = "cheeseburger";
     $scope.members = [];
     $scope.member = '';
     $scope.total = 179;
     $scope.isLess = true;
+    $scope.promocode = "Promo Code";
 
     $scope.addMember = function(){
       console.log('submitting');
@@ -12,10 +13,26 @@ myApp.controller('AppCtrl', ['$scope',
         $scope.members.push(this.member);
         $scope.member = '';
         $scope.total += 50;
+
         if($scope.members.length === 3){
           $scope.isLess = false;
         }
       }
     };
+
+    $scope.addPromocode = function(){
+      $http.get('data/promo.json').success(function(data) {
+        for(let promocode of data){
+          if($scope.promocode === promocode.code){
+            console.log("here ", promocode);
+          }
+        }
+      })
+      .error(function(data){
+        console.log("ERROR");
+      });
+    };
+
+
 
 }]);
