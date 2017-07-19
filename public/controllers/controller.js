76f03be63b2004d42e04dde891c945361d3b5968
Viswada Yangala <yangalav@go.stockton.edu>
2017-07-19 01:51:00 -0400

    can add members using family member modal

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,4 +1,15 @@
 myApp.controller('AppCtrl', ['$scope',
   function($scope) {
     $scope.haha = "cheeseburger";
+    $scope.members = [];
+    $scope.member = '';
+
+    $scope.addMember = function(){
+      console.log('submitting');
+      if($scope.member) {
+        $scope.members.push(this.member);
+        $scope.member = '';
+      }
+    };
+
 }]);
