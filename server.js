6158a4dd970fd131b66d1365f67e506f8b42a91a
Viswada Yangala <yangalav@go.stockton.edu>
2017-07-23 17:25:05 -0400

    package.json

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use(express.static('public/'));
 
-app.listen(process.env, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port 3000");
 });
